f0a00084c1e033b4512b71ad890e652c4ac18257
MINS <kmscsem1@gmail.com>
2018-09-22 16:00:22 +0900

    parameter 입력

--- src/main/java/kr/co/mins/app/TestApp.java
+++ src/main/java/kr/co/mins/app/TestApp.java
@@ -2,7 +2,10 @@ package kr.co.mins.app;
 
 public class TestApp {
     public static void main (String[] args){
-        TestExample model = new TestExample()
+
+        TestExample model = new TestExample(5, 2, 3, 45, 5, 2, 3, 4, 1, 3);
+
+
     }
 
 
